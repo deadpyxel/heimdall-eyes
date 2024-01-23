0853fc9728aa91e1c38d169629f4be1cc00c6b59
Robson Cruz <ghost0092@gmail.com>
2024-01-23 18:37:34 -0300

    feat(cmd/main.go): update entrypoint
    Using new implementation for the monitoring loop to validate the current
    approach

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/deadpyxel/heimdall-eyes/pkg/models"
 	"github.com/deadpyxel/heimdall-eyes/pkg/monitor"
 	"github.com/deadpyxel/heimdall-eyes/pkg/services"
 )
@@ -11,5 +16,28 @@ func main() {
 	svcManager := services.ServiceManager{}
 	httpMonitor := monitor.HTTPMonitor{}
 
-	fmt.Printf("%v\n%v", svcManager, httpMonitor)
+	// Example service
+	service := models.Service{
+		ID:           1,
+		Name:         "Example Service",
+		URL:          "https://github.com",
+		SuccessCodes: []int{200},
+		Interval:     30 * time.Second,
+	}
+
+	// Add service to the manager
+	if err := svcManager.AddService(service); err != nil {
+		fmt.Printf("Error adding service: %v\n", err)
+		return
+	}
+
+	done := make(chan struct{})
+	go httpMonitor.StartMonitoring(service, done)
+
+	// Graceful shutdown handling
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+	fmt.Println("Shutting down...")
+	close(done)
 }
