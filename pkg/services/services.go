1dffa675a11b3de3768a6be4ca5cb7c38ab7c8b9
Robson Cruz <ghost0092@gmail.com>
2024-01-23 18:27:23 -0300

    feat(pkg/services): add control logic for Manager
    Now adding and removing services should be possible
    
    Maybe we should rename the ServiceManager struct to ServicePool as it is
    basically a container right now

--- pkg/services/services.go
+++ pkg/services/services.go
@@ -1,6 +1,10 @@
 package services
 
-import "github.com/deadpyxel/heimdall-eyes/pkg/models"
+import (
+	"fmt"
+
+	"github.com/deadpyxel/heimdall-eyes/pkg/models"
+)
 
 type MonitorController interface {
 	AddService(svc models.Service) error
@@ -11,4 +15,22 @@ type MonitorController interface {
 
 type ServiceManager struct {
 	services []models.Service
+}
+
+func (sm *ServiceManager) AddService(svc models.Service) error {
+	if err := svc.Validate(); err != nil {
+		return err
+	}
+	sm.services = append(sm.services, svc)
+	return nil
+}
+
+func (sm *ServiceManager) RemoveService(id int) error {
+	for i, svc := range sm.services {
+		if svc.ID == id {
+			sm.services = append(sm.services[:i], sm.services[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Service with ID %d not found", id)
 }
